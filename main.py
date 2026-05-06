55638a0bd507c69284aec85da5d43917a2f03e66
AnjaliPriyadarshi <anjalipriyadarshi20@gmail.com>
2026-05-06 07:02:52 +0000

    deplot trial 1

--- main.py
+++ main.py
@@ -348,4 +348,5 @@ if __name__ == "__main__":
     else:
         print("\nStarting production backend server...")
         import uvicorn
-        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+        port = int(os.environ.get("PORT", 8000))
+        uvicorn.run(app, host="0.0.0.0", port=port)
